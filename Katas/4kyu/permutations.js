85f6dbdb903fd7b210b2632aa8d8e63cfb2828fe
AaronPBishop <aaronpbishop@gmail.com>
2022-10-13 14:08:14 -0500

    Refactored 'permutations.js - 4kyu' to recurse more accurately given the number of total possible permutations of an input

--- Katas/4kyu/permutations.js
+++ Katas/4kyu/permutations.js
@@ -11,7 +11,51 @@
 
 // The order of the permutations doesn't matter.
 
-const permutations = (string, finalArray = [], i = 0, visited =  new Set()) => {
+const hasDuplicates = (str) => {
+    const letters = {};
+    str.split('').forEach(letter => {
+        if (letters[letter] !== undefined) letters[letter]++;
+        if (letters[letter] === undefined) letters[letter] = 1;
+    });
+
+    for (let key in letters) {
+        let currLetter = letters[key];
+        if (currLetter > 1) return letters;
+    };
+
+    return false;
+};
+
+const numPermutations = (str) => {
+    if (hasDuplicates(str) === false) {
+        let multiplier = 1;
+
+        for (let i = 1; i <= str.length; i++) multiplier *= i;
+
+        return multiplier;
+    };
+
+    const letters = hasDuplicates(str);
+    let multiplier = 1;
+    let divisor = 1;
+    let visited = new Set();
+
+    for (let i = 1; i <= str.length; i++) {
+        let currLetter = str[i];
+
+        if (letters[currLetter] > 1 && !visited.has(currLetter)) {
+            for (let j = 1; j <= letters[currLetter]; j++) {
+                divisor *= j;
+            };
+            visited.add(currLetter);
+        };
+        multiplier *= i;
+    };
+
+    return (multiplier / divisor);
+};
+
+const permutations = (string, finalArray = [], i = 0, visited = new Set()) => {
     if (string.length === 2) {
         if (string.split('')[0] === string.split('')[1]) {
             finalArray.push(string);
@@ -20,7 +64,9 @@ const permutations = (string, finalArray = [], i = 0, visited =  new Set()) => {
     };
 
 	if (string.length <= 2 && finalArray.length === string.length) return finalArray;
-    if (i === 5000) return finalArray;
+
+    const maxPermutations = numPermutations(string);
+    if (i === maxPermutations) return finalArray;
 
     const splitString = string.split('');
 
@@ -32,8 +78,12 @@ const permutations = (string, finalArray = [], i = 0, visited =  new Set()) => {
         splitString[randNum] = replacement;
     };
 
-    if (!visited.has(splitString.join(''))) finalArray.push(splitString.join(''));
-    visited.add(splitString.join(''));
+    if (!visited.has(splitString.join(''))) {
+        finalArray.push(splitString.join(''));
+        visited.add(splitString.join(''));
 
-    return permutations(string, finalArray, i += 1, visited);
+        return permutations(string, finalArray, i += 1, visited);
+    };
+
+    return permutations(string, finalArray, i, visited);
 };
