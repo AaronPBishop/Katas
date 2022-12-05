d81bc994975ddad5df4db14de4634aaf6586d1f2
AaronPBishop <aaronpbishop@gmail.com>
2022-12-05 15:34:37 -0600

    Completed recursive solution to snail.py - 4kyu

--- Katas/Python/4kyu/snail.py
+++ Katas/Python/4kyu/snail.py
@@ -16,18 +16,39 @@
 
 # NOTE 2: The 0x0 (empty matrix) is represented as en empty list inside a list [[]].
 
-def snail(ls):
+def snail(ls, final_ls=None, i=0, j=0):
+    if final_ls is None:
+        final_ls = []
     if len(ls) < 1:
-        return ls
+        return final_ls
+    
+    if i==0:
+        if len(ls) > 1 and j > 0:
+            ls.reverse()
+            for curr_ls in ls:
+                final_ls.append(curr_ls[0])
+                curr_ls.pop(0)
+            ls.reverse()
 
-    final_ls = []
-    for curr_ls in ls:
-        for curr_el in curr_ls:
-            pass
+        for el in ls[0]:
+            final_ls.append(el)
+        ls.pop(0)
+
+    if i==1:
+        for curr_ls in ls:
+            final_ls.append(curr_ls.pop())
+    
+    if i==2:
+        bottom_row = ls[len(ls) - 1]
+        bottom_row.reverse()
+
+        for el in bottom_row:
+            final_ls.append(el)
             
+        ls.pop()
 
-snail_list = [[1,2,3],
-              [8,9,4],
-              [7,6,5]]
+        return snail(ls, final_ls, i=0, j=j+1)
 
-print(snail(snail_list)) #=> [1,2,3,4,5,6,7,8,9]
+    i+=1
+    j+=1
+    return snail(ls, final_ls, i, j)
