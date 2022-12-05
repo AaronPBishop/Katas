4bfcca1693066c13275c6d679c4ebede05a1ad97
AaronPBishop <aaronpbishop@gmail.com>
2022-12-04 22:48:19 -0600

    Unsaved changes in snail.py - 4kyu

--- Katas/Python/4kyu/snail.py
+++ Katas/Python/4kyu/snail.py
@@ -1,22 +1,33 @@
-# Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+# Given an n x n list, return the list elements arranged from outermost elements to the middle element, traveling clockwise.
 
-# array = [[1,2,3],
+# list = [[1,2,3],
 #          [4,5,6],
 #          [7,8,9]]
-# snail(array) #=> [1,2,3,6,9,8,7,4,5]
+# snail(list) #=> [1,2,3,6,9,8,7,4,5]
 
-# For better understanding, please follow the numbers of the next array consecutively:
+# For better understanding, please follow the numbers of the next list consecutively:
 
-# array = [[1,2,3],
+# list = [[1,2,3],
 #          [8,9,4],
 #          [7,6,5]]
-# snail(array) #=> [1,2,3,4,5,6,7,8,9]
+# snail(list) #=> [1,2,3,4,5,6,7,8,9]
 
-# This image will illustrate things more clearly:
+# NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d list in a clockwise snailshell pattern.
 
-# NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+# NOTE 2: The 0x0 (empty matrix) is represented as en empty list inside a list [[]].
 
-# NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+def snail(ls):
+    if len(ls) < 1:
+        return ls
 
-def snail(arr):
-    pass
+    final_ls = []
+    for curr_ls in ls:
+        for curr_el in curr_ls:
+            pass
+            
+
+snail_list = [[1,2,3],
+              [8,9,4],
+              [7,6,5]]
+
+print(snail(snail_list)) #=> [1,2,3,4,5,6,7,8,9]
