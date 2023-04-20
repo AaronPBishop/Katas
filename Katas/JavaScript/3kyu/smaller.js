c0860e01027d5f746ecb94716a9ff2ff96556c77
AaronPBishop <aaronpbishop@gmail.com>
2023-04-20 17:38:04 -0500

    Added 'checkPipe.js' - 3kyu

--- Katas/JavaScript/3kyu/smaller.js
+++ Katas/JavaScript/3kyu/smaller.js
@@ -1,7 +1,5 @@
 // This is a hard version of How many are smaller than me?. If you have troubles solving this one, have a look at the easier kata first.
 
-// Write
-
 // function smaller(arr)
 
 // that given an array arr, you have to return the amount of numbers that are smaller than arr[i] to the right.
@@ -11,8 +9,7 @@
 // smaller([5, 4, 3, 2, 1]) === [4, 3, 2, 1, 0]
 // smaller([1, 2, 0]) === [1, 1, 0]
 
-const test = [5, 4, 3, 2, 1];
-// [4, 3, 2, 1, 0]
+// Given extremely large inputs (90k+), your code will have 12000 ms to complete execution.
 
 const merge = (arr, indices, left, mid, right, res) => {
     const tmp = [];
@@ -50,9 +47,7 @@ const mergeSort = (arr, indices, left, right, res) => {
 const smaller = (arr) => {
     const res = Array(arr.length).fill(0);
     const indices = arr.map((_, i) => i);
-    
+
     mergeSort(arr, indices, 0, arr.length - 1, res);
     return res;
 };
-
-console.log(smaller(test));
