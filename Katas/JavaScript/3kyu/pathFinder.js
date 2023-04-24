4294541a5887772e75f5c0d5366d3516724ac804
AaronPBishop <aaronpbishop@gmail.com>
2023-04-23 19:58:13 -0500

    Partial solution to 'pathFinder.js' - 4kyu completed

--- Katas/JavaScript/3kyu/pathFinder.js
+++ Katas/JavaScript/3kyu/pathFinder.js
@@ -4,11 +4,50 @@ You are at start location [0, 0] in mountain area of NxN and you can only move i
 Location altitude is defined as an integer number (0-9).
 */
 
+const getNeighbors = (grid, currPos) => {
+    const [row, col] = currPos;
+    const neighbors = [
+        [row - 1, col],
+        [row + 1, col],
+        [row, col - 1],
+        [row, col + 1]
+    ];
+
+    return neighbors.filter(neighbor => {
+        const [nRow, nCol] = neighbor;
+
+        if (grid[nRow] !== undefined && grid[nRow][nCol] !== undefined) return neighbor;
+    });
+};
+
 const pathFinder = (area) => {
     const grid = [[]];
-    area.split('').forEach(el => el === '\n' ? grid.push([]) : grid[grid.length - 1].push(el));
+    area.split('').forEach(el => el === '\n' ? grid.push([]) : grid[grid.length - 1].push(Number(el)));
 
-    return grid;
+    const distances = {};
+    const queue = [{ pos: [0, 0], sum: 0 }];
+    const visited = new Set([`${0}-${0}`]);
+
+    while (queue.length) {
+        const { pos, sum } = queue.shift();
+        const [row, col] = pos;
+        const key = `${row}-${col}`;
+
+        distances[key] = sum;
+
+        const neighbors = getNeighbors(grid, pos);
+        for (let neighbor of neighbors) {
+            const [nRow, nCol] = neighbor;
+            const nKey = `${nRow}-${nCol}`;
+
+            if (!visited.has(nKey)) {
+                visited.add(nKey);
+                queue.push({ pos: neighbor, sum: sum + Math.abs(grid[row][col] - grid[nRow][nCol])});
+            };
+        };
+    };
+
+    return distances[`${grid.length - 1}-${grid.length - 1}`];
 };
 
 const testArea = 
@@ -16,4 +55,4 @@ const testArea =
 101
 010`; // 4
 
-console.log(pathFinder(testArea))
+console.log(pathFinder(testArea));
