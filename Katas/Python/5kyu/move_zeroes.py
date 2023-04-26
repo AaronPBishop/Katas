5d7afe769f00dcf6602c3187b4d563ed7d07f23a
AaronPBishop <aaronpbishop@gmail.com>
2023-04-26 13:51:38 -0500

    Completed 'move_zeroes.py' - 5kyu

--- Katas/Python/5kyu/move_zeroes.py
+++ Katas/Python/5kyu/move_zeroes.py
@@ -3,4 +3,10 @@
 # move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
 
 def move_zeros(lst):
-    return lst
+    zeroes = [el for el in lst if el == 0]
+    filtered = [el for el in lst if el != 0]
+    filtered.extend(zeroes)
+    
+    return filtered
+
+print(move_zeros([1, 0, 1, 2, 0, 1, 3]))
