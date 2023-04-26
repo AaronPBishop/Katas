edeeabfa9958f264f04ae78025423cc9fa7332de
AaronPBishop <aaronpbishop@gmail.com>
2023-04-26 15:36:21 -0500

    Solved 'find_uniq.py' - 5kyu

--- Katas/Python/5kyu/find_uniq.py
+++ Katas/Python/5kyu/find_uniq.py
@@ -7,7 +7,42 @@
 
 # It’s guaranteed that array contains more than 2 strings.
 
-def find_uniq(arr):
-    return None
+def generate_dict(ls):
+    str_dict = {}
+    for str in ls:
+        if not str:
+            if 'empty' in str_dict:
+                str_dict['empty'] += 1
+            else:
+                str_dict['empty'] = 1
+            continue
 
-print(find_uniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]))
+        if all(char.lower() == str[0].lower() for char in str):
+            if str[0].lower() in str_dict:
+                str_dict[str[0].lower()] += 1
+            else:
+                str_dict[str[0].lower()] = 1
+            continue
+
+        cleaned_str = ''.join(sorted(str.lower())).strip()
+        if cleaned_str in str_dict:
+            str_dict[cleaned_str] += 1
+        else:
+            str_dict[cleaned_str] = 1
+    return [str_dict, min(str_dict.values())]
+
+
+def find_uniq(ls):
+    [str_dict, min_value] = generate_dict(ls)
+    for str in ls:
+        for key, val in str_dict.items():
+            if not str:
+                if key == 'empty' and val == min_value:
+                    return str
+                else: 
+                    continue
+            if all(char.lower() == str[0].lower() for char in str):
+                if str[0].lower() == key and val == min_value:
+                    return str
+            if ''.join(sorted(str.lower())).strip() == key and val == min_value:
+                return str
