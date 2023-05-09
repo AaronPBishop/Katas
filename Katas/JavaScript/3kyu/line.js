22066674b00b33bf02f1ee39376b685024deacf8
AaronPBishop <aaronpbishop@gmail.com>
2023-05-09 18:26:12 -0500

    Made progress with 'line.js' - 3kyu

--- Katas/JavaScript/3kyu/line.js
+++ Katas/JavaScript/3kyu/line.js
@@ -79,3 +79,114 @@ Hint
 
 Imagine yourself walking a path where you can only see your very next step. Can you know which step you must take, or not?
 */
+
+const findStartPoint = (grid) => {
+   for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+         if (grid[i][j] === 'X') return [i, j];
+      };
+   };
+};
+
+const getNeighbors = (grid, currPos) => {
+   const [currRow, currCol] = currPos;
+
+   const neighbors = [
+      [currRow - 1, currCol],
+      [currRow + 1, currCol],
+      [currRow, currCol - 1],
+      [currRow, currCol + 1]
+   ];
+
+   const validNeighbors = [];
+   for (let nbr of neighbors) {
+      const [nRow, nCol] = nbr;
+
+      if (nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length) {
+         if (grid[nRow][nCol] !== ' ') validNeighbors.push([nRow, nCol]);
+      };
+   };
+
+   return validNeighbors;
+};
+
+const mayConnect = (currEl, nextEl) => {
+   const mapConnections = {
+      'X': {
+         'LEFT': ['-'],
+         'RIGHT': ['-'],
+         'UP': ['|'],
+         'DOWN': ['|']
+      },
+      '|': {
+         'UP': ['|', '+', 'X'],
+         'DOWN': ['|', '+', 'X']
+      },
+      '-': {
+         'LEFT': ['-', '+', 'X'],
+         'RIGHT': ['-', '+', 'X']
+      },
+      '+': {
+         'LEFT': ['-', '+'],
+         'RIGHT': ['-', '+'],
+         'UP': ['|', '+'],
+         'DOWN': ['|', '+']
+      }
+   };
+
+   for (let key in mapConnections[currEl]) {
+      const currDir = mapConnections[currEl][key];
+
+      for (let line of currDir) {
+         if (line === nextEl) return true;
+      };
+   };
+
+   return false;
+};
+
+const line = (grid) => {
+   const [startX, startY] = findStartPoint(grid);
+
+   const queue = [[startX, startY]];
+   const visited = new Set([`${startX}-${startY}`]);
+
+   while (queue.length) {
+      const [currRow, currCol] = queue.shift();
+
+      const neighbors = getNeighbors(grid, [currRow, currCol]);
+      if (neighbors.length <= 1 && grid[currRow][currCol] !== 'X') return false;
+
+      for (let nbr of neighbors) {
+         const [nRow, nCol] = nbr;
+         const coord = `${nRow}-${nCol}`;
+
+         if (!visited.has(coord)) {
+            if (!mayConnect(grid[currRow][currCol].toString(), grid[nRow][nCol].toString())) return false;
+
+            queue.push(nbr);
+            visited.add(coord);
+         };
+      };
+   };
+
+   return true;
+};
+
+const mockGrid1 = [ 
+   [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
+   [ ' ', ' ', ' ', '+', '-', '-', '-', '-', '-', '-', ' ', ' ', ' ', ' ' ],
+   [ ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
+   [ 'X', '-', '-', '+', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ' ],
+   [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ] 
+]
+
+const mockGrid2 = [ 
+   [ ' ', ' ', ' ', ' ', ' ' ],
+   [ ' ', ' ', 'X', ' ', ' ' ],
+   [ ' ', ' ', '|', ' ', ' ' ],
+   [ ' ', ' ', '|', ' ', ' ' ],
+   [ ' ', ' ', 'X', ' ', ' ' ] 
+];
+
+console.log(line(mockGrid1));
