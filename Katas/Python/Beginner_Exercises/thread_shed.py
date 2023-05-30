03ea7a93201417f14195049a4f28aaf78fcc094e
AaronPBishop <aaronpbishop@gmail.com>
2023-05-30 14:06:07 -0500

    Completed 'thread_shed.py'

---
+++ Katas/Python/Beginner_Exercises/thread_shed.py
@@ -0,0 +1,177 @@
+# You’ve recently been hired as a cashier at the local sewing hobby shop, Thread Shed. Some of your daily responsibilities involve tallying the number of sales during the day, calculating the total amount of money made, and keeping track of the names of the customers.
+
+# Unfortunately, the Thread Shed has an extremely outdated register system and stores all of the transaction information in one huge unwieldy string called daily_sales.
+
+# All day, for each transaction, the name of the customer, amount spent, types of thread purchased, and the date of sale is all recorded in this same string. Your task is to use your Python skills to iterate through this string and clean up each transaction and store all the information in easier-to-access lists.
+
+daily_sales = \
+"""Edith Mcbride   ;,;$1.21   ;,;   white ;,; 
+09/15/17   ,Herbert Tran   ;,;   $7.29;,; 
+white&blue;,;   09/15/17 ,Paul Clarke ;,;$12.52 
+;,;   white&blue ;,; 09/15/17 ,Lucille Caldwell   
+;,;   $5.13   ;,; white   ;,; 09/15/17,
+Eduardo George   ;,;$20.39;,; white&yellow 
+;,;09/15/17   ,   Danny Mclaughlin;,;$30.82;,;   
+purple ;,;09/15/17 ,Stacy Vargas;,; $1.85   ;,; 
+purple&yellow ;,;09/15/17,   Shaun Brock;,; 
+$17.98;,;purple&yellow ;,; 09/15/17 , 
+Erick Harper ;,;$17.41;,; blue ;,; 09/15/17, 
+Michelle Howell ;,;$28.59;,; blue;,;   09/15/17   , 
+Carroll Boyd;,; $14.51;,;   purple&blue   ;,;   
+09/15/17   , Teresa Carter   ;,; $19.64 ;,; 
+white;,;09/15/17   ,   Jacob Kennedy ;,; $11.40   
+;,; white&red   ;,; 09/15/17, Craig Chambers;,; 
+$8.79 ;,; white&blue&red   ;,;09/15/17   , Peggy Bell;,; $8.65 ;,;blue   ;,; 09/15/17,   Kenneth Cunningham ;,;   $10.53;,;   green&blue   ;,; 
+09/15/17   ,   Marvin Morgan;,;   $16.49;,; 
+green&blue&red   ;,;   09/15/17 ,Marjorie Russell 
+;,; $6.55 ;,;   green&blue&red;,;   09/15/17 ,
+Israel Cummings;,;   $11.86   ;,;black;,;  
+09/15/17,   June Doyle   ;,;   $22.29 ;,;  
+black&yellow ;,;09/15/17 , Jaime Buchanan   ;,;   
+$8.35;,;   white&black&yellow   ;,;   09/15/17,   
+Rhonda Farmer;,;$2.91 ;,;   white&black&yellow   
+;,;09/15/17, Darren Mckenzie ;,;$22.94;,;green 
+;,;09/15/17,Rufus Malone;,;$4.70   ;,; green&yellow 
+;,; 09/15/17   ,Hubert Miles;,;   $3.59   
+;,;green&yellow&blue;,;   09/15/17   , Joseph Bridges  ;,;$5.66   ;,; green&yellow&purple&blue 
+;,;   09/15/17 , Sergio Murphy   ;,;$17.51   ;,;   
+black   ;,;   09/15/17 , Audrey Ferguson ;,; 
+$5.54;,;black&blue   ;,;09/15/17 ,Edna Williams ;,; 
+$17.13;,; black&blue;,;   09/15/17,   Randy Fleming;,;   $21.13 ;,;black ;,;09/15/17 ,Elisa Hart;,; $0.35   ;,; black&purple;,;   09/15/17   ,
+Ernesto Hunt ;,; $13.91   ;,;   black&purple ;,;   
+09/15/17,   Shannon Chavez   ;,;$19.26   ;,; 
+yellow;,; 09/15/17   , Sammy Cain;,; $5.45;,;   
+yellow&red ;,;09/15/17 ,   Steven Reeves ;,;$5.50   
+;,;   yellow;,;   09/15/17, Ruben Jones   ;,; 
+$14.56 ;,;   yellow&blue;,;09/15/17 , Essie Hansen;,;   $7.33   ;,;   yellow&blue&red
+;,; 09/15/17   ,   Rene Hardy   ;,; $20.22   ;,; 
+black ;,;   09/15/17 ,   Lucy Snyder   ;,; $8.67   
+;,;black&red  ;,; 09/15/17 ,Dallas Obrien ;,;   
+$8.31;,;   black&red ;,;   09/15/17,   Stacey Payne 
+;,;   $15.70   ;,;   white&black&red ;,;09/15/17   
+,   Tanya Cox   ;,;   $6.74   ;,;yellow   ;,; 
+09/15/17 , Melody Moran ;,;   $30.84   
+;,;yellow&black;,;   09/15/17 , Louise Becker   ;,; 
+$12.31 ;,; green&yellow&black;,;   09/15/17 ,
+Ryan Webster;,;$2.94 ;,; yellow ;,; 09/15/17 
+,Justin Blake ;,; $22.46   ;,;white&yellow ;,;   
+09/15/17,   Beverly Baldwin ;,;   $6.60;,;   
+white&yellow&black ;,;09/15/17   ,   Dale Brady   
+;,;   $6.27 ;,; yellow   ;,;09/15/17 ,Guadalupe Potter ;,;$21.12   ;,; yellow;,; 09/15/17   , 
+Desiree Butler ;,;$2.10   ;,;white;,; 09/15/17  
+,Sonja Barnett ;,; $14.22 ;,;white&black;,;   
+09/15/17, Angelica Garza;,;$11.60;,;white&black   
+;,;   09/15/17   ,   Jamie Welch   ;,; $25.27   ;,; 
+white&black&red ;,;09/15/17   ,   Rex Hudson   
+;,;$8.26;,;   purple;,; 09/15/17 ,   Nadine Gibbs 
+;,;   $30.80 ;,;   purple&yellow   ;,; 09/15/17   , 
+Hannah Pratt;,;   $22.61   ;,;   purple&yellow   
+;,;09/15/17,Gayle Richards;,;$22.19 ;,; 
+green&purple&yellow ;,;09/15/17   ,Stanley Holland 
+;,; $7.47   ;,; red ;,; 09/15/17 , Anna Dean;,;$5.49 ;,; yellow&red ;,;   09/15/17   ,
+Terrance Saunders ;,;   $23.70  ;,;green&yellow&red 
+;,; 09/15/17 ,   Brandi Zimmerman ;,; $26.66 ;,; 
+red   ;,;09/15/17 ,Guadalupe Freeman ;,; $25.95;,; 
+green&red ;,;   09/15/17   ,Irving Patterson 
+;,;$19.55 ;,; green&white&red ;,;   09/15/17 ,Karl Ross;,;   $15.68;,;   white ;,;   09/15/17 , Brandy Cortez ;,;$23.57;,;   white&red   ;,;09/15/17, 
+Mamie Riley   ;,;$29.32;,; purple;,;09/15/17 ,Mike Thornton   ;,; $26.44 ;,;   purple   ;,; 09/15/17, 
+Jamie Vaughn   ;,; $17.24;,;green ;,; 09/15/17   , 
+Noah Day ;,;   $8.49   ;,;green   ;,;09/15/17   
+,Josephine Keller ;,;$13.10 ;,;green;,;   09/15/17 ,   Tracey Wolfe;,;$20.39 ;,; red   ;,; 09/15/17 ,
+Ignacio Parks;,;$14.70   ;,; white&red ;,;09/15/17 
+, Beatrice Newman ;,;$22.45   ;,;white&purple&red 
+;,;   09/15/17, Andre Norris   ;,;   $28.46   ;,;   
+red;,;   09/15/17 ,   Albert Lewis ;,; $23.89;,;   
+black&red;,; 09/15/17,   Javier Bailey   ;,;   
+$24.49   ;,; black&red ;,; 09/15/17   , Everett Lyons ;,;$1.81;,;   black&red ;,; 09/15/17 ,   
+Abraham Maxwell;,; $6.81   ;,;green;,;   09/15/17   
+,   Traci Craig ;,;$0.65;,; green&yellow;,; 
+09/15/17 , Jeffrey Jenkins   ;,;$26.45;,; 
+green&yellow&blue   ;,;   09/15/17,   Merle Wilson 
+;,;   $7.69 ;,; purple;,; 09/15/17,Janis Franklin   
+;,;$8.74   ;,; purple&black   ;,;09/15/17 ,  
+Leonard Guerrero ;,;   $1.86   ;,;yellow  
+;,;09/15/17,Lana Sanchez;,;$14.75   ;,; yellow;,;   
+09/15/17   ,Donna Ball ;,; $28.10  ;,; 
+yellow&blue;,;   09/15/17   , Terrell Barber   ;,; 
+$9.91   ;,; green ;,;09/15/17   ,Jody Flores;,; 
+$16.34 ;,; green ;,;   09/15/17,   Daryl Herrera 
+;,;$27.57;,; white;,;   09/15/17   , Miguel Mcguire;,;$5.25;,; white&blue   ;,;   09/15/17 ,   
+Rogelio Gonzalez;,; $9.51;,;   white&black&blue   
+;,;   09/15/17   ,   Lora Hammond ;,;$20.56 ;,; 
+green;,;   09/15/17,Owen Ward;,; $21.64   ;,;   
+green&yellow;,;09/15/17,Malcolm Morales ;,;   
+$24.99   ;,;   green&yellow&black;,; 09/15/17 ,   
+Eric Mcdaniel ;,;$29.70;,; green ;,; 09/15/17 
+,Madeline Estrada;,;   $15.52;,;green;,;   09/15/17 
+, Leticia Manning;,;$15.70 ;,; green&purple;,; 
+09/15/17 ,   Mario Wallace ;,; $12.36 ;,;green ;,; 
+09/15/17,Lewis Glover;,;   $13.66   ;,;   
+green&white;,;09/15/17,   Gail Phelps   ;,;$30.52   
+;,; green&white&blue   ;,; 09/15/17 , Myrtle Morris 
+;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
+
+def parse_data(sales_data):
+    split_sales = sales_data.split(';,;')
+    cleaned_data = [[]]
+
+    for sale in split_sales:
+        curr_item = sale.split(',')
+
+        if len(curr_item) > 1:
+            cleaned_data[-1].append(curr_item[0].strip())
+            cleaned_data.append([])
+            cleaned_data[-1].append(curr_item[1].strip())
+
+            continue
+
+        cleaned_data[-1].append(curr_item[0].strip())
+
+
+    return cleaned_data
+
+def organize_data(sales_data):
+    sales_dict = { 'customers': [], 'profits': [], 'colors_sold': [], 'date': '' }
+    for sale in sales_data:
+        customer, profit, color, curr_date = sale
+
+        sales_dict['customers'].append(customer)
+        sales_dict['profits'].append(profit)
+        sales_dict['colors_sold'].append(color)
+
+        if not sales_dict['date']: sales_dict['date'] = curr_date
+
+    return sales_dict
+
+def determine_profits(profit_data):
+    sum = 0.0
+    for profit in profit_data:
+        cleaned_profit = float(profit.split('$').pop())
+        sum += cleaned_profit
+    return round(sum, 2)
+
+def determine_colors_sold(colors_data):
+    colors_sold = {}
+    for color in colors_data:
+        curr_colors = color.split('&')
+
+        if len(curr_colors) > 1:
+            for curr_color in curr_colors:
+                if curr_color in colors_sold: colors_sold[curr_color] += 1
+                else: colors_sold[curr_color] = 1
+            continue
+
+        if curr_colors[0] in colors_sold: colors_sold[curr_colors[0]] += 1
+        else: colors_sold[curr_colors[0]] = 1
+
+    return colors_sold
+
+
+def get_stats(sales_data):
+    parsed_data = parse_data(sales_data)
+    organized_data = organize_data(parsed_data)
+
+    daily_profit = determine_profits(organized_data['profits'])
+    colors_sold = determine_colors_sold(organized_data['colors_sold'])
+
+    return { 'total_customers': len(organized_data['customers']), 'daily_profit': daily_profit, 'colors_sold': colors_sold, 'date': organized_data['date'] }
